a99a4cc152bc72be70a5dac6c0c68ec6542aa22f
Anna Do <annado@gmail.com>
2014-03-20 23:51:40 -0700

    Initial Commit

---
+++ Yelply/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Yelply
+//
+//  Created by Anna Do on 3/20/14.
+//  Copyright (c) 2014 Anna Do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
